84d1e951aa4e1a8e38af0d35a7be21407e8508c8
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2019-01-07 08:04:39 +0100

    fix(alerter): fix details display for sws batch result
    ref: #21

--- src/alerter/alerter-directive.js
+++ src/alerter/alerter-directive.js
@@ -32,6 +32,10 @@ export default function () {
         $scope.ovhAlertMessageDetails = null;
       };
 
+      $scope.expandDetails = function () {
+        $scope.expand = !$scope.expand;
+      };
+
       if ($attr.ovhAlertHideRemoveButton !== undefined) {
         $scope.ovhAlertHideRemoveButton = true;
       }
